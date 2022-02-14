97454077c39da18b23c7ee1451cdea345b5b52be
Hari Nur Permadi <hari.permadi@prudential.co.id>
2022-02-14 11:54:04 +0700

    Initial Commit

---
+++ CupCakeCorner/CupCakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupCakeCornerApp.swift
+//  CupCakeCorner
+//
+//  Created by Hari Permadi on 14/02/22.
+//
+
+import SwiftUI
+
+@main
+struct CupCakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
